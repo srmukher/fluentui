3f259c366611a2ed5c7e5866fd7c4a6bdd5cbc4f
srmukher <srmukher@microsoft.com>
2023-02-09 17:50:18 +0530

    Adding snapshot tests for functions

--- packages/react-charting/src/components/DonutChart/DonutChart.test.tsx
+++ packages/react-charting/src/components/DonutChart/DonutChart.test.tsx
@@ -43,7 +43,10 @@ const chartPoints: IChartProps = {
 
 describe('DonutChart snapShot testing', () => {
   it('renders DonutChart correctly', () => {
+    // Arrange
     const component = renderer.create(<DonutChart data={chartPoints} innerRadius={55} />);
+
+    // Assert
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
@@ -55,19 +58,28 @@ describe('DonutChart snapShot testing', () => {
   });
 
   it('renders hideTooltip correctly', () => {
+    // Arrange
     const component = renderer.create(<DonutChart data={chartPoints} hideTooltip={true} />);
+
+    // Assert
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('renders enabledLegendsWrapLines correctly', () => {
+    // Arrange
     const component = renderer.create(<DonutChart data={chartPoints} enabledLegendsWrapLines={true} />);
+
+    // Assert
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('renders value inside onf the pie', () => {
+    // Arrange
     const component = renderer.create(<DonutChart data={chartPoints} valueInsideDonut={1000} />);
+
+    // Assert
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
@@ -78,24 +90,34 @@ describe('DonutChart - basic props', () => {
   afterEach(sharedAfterEach);
 
   it('Should mount legend when hideLegend false ', () => {
+    // Arrange
     wrapper = mount(<DonutChart data={chartPoints} />);
+
+    // Assert
     const hideLegendDOM = wrapper.getDOMNode().querySelectorAll('[class^="legendContainer"]');
     expect(hideLegendDOM).toBeDefined();
   });
 
   it('Should mount callout when hideTootip false ', () => {
+    // Arrange
     wrapper = mount(<DonutChart data={chartPoints} />);
+
+    // Assert
     const hideLegendDOM = wrapper.getDOMNode().querySelectorAll('[class^="ms-Layer"]');
     expect(hideLegendDOM).toBeDefined();
   });
 
   it('Should not render onRenderCalloutPerStack ', () => {
+    // Arrange
     wrapper = mount(<DonutChart data={chartPoints} />);
+
+    // Assert
     const renderedDOM = wrapper.getDOMNode().getElementsByClassName('.onRenderCalloutPerStack');
     expect(renderedDOM!.length).toBe(0);
   });
 
   it('Should render onRenderCalloutPerDataPoint ', () => {
+    // Arrange
     wrapper = mount(
       <DonutChart
         data={chartPoints}
@@ -108,12 +130,17 @@ describe('DonutChart - basic props', () => {
         }
       />,
     );
+
+    // Assert
     const renderedDOM = wrapper.getDOMNode().getElementsByClassName('.onRenderCalloutPerDataPoint');
     expect(renderedDOM).toBeDefined();
   });
 
   it('Should not render onRenderCalloutPerDataPoint ', () => {
+    // Arrange
     wrapper = mount(<DonutChart data={chartPoints} />);
+
+    // Assert
     const renderedDOM = wrapper.getDOMNode().getElementsByClassName('.onRenderCalloutPerDataPoint');
     expect(renderedDOM!.length).toBe(0);
   });
@@ -124,23 +151,34 @@ describe('DonutChart - mouse events', () => {
   afterEach(sharedAfterEach);
 
   it('Should render callout correctly on mouseover', () => {
+    // Arrange
     wrapper = mount(<DonutChart data={chartPoints} innerRadius={55} calloutProps={{ doNotLayer: true }} />);
+
+    // Act
     wrapper.find('path[id^="_Pie_"]').at(0).simulate('mouseover');
+
+    // Assert
     const tree = toJson(wrapper, { mode: 'deep' });
     expect(tree).toMatchSnapshot();
   });
 
   it('Should render callout correctly on mousemove', () => {
+    // Arrange
     wrapper = mount(<DonutChart data={chartPoints} innerRadius={55} calloutProps={{ doNotLayer: true }} />);
+
+    // Act
     wrapper.find('path[id^="_Pie_"]').at(0).simulate('mousemove');
     const html1 = wrapper.html();
     wrapper.find('path[id^="_Pie_"]').at(0).simulate('mouseleave');
     wrapper.find('path[id^="_Pie_"]').at(1).simulate('mousemove');
     const html2 = wrapper.html();
+
+    // Assert
     expect(html1).not.toBe(html2);
   });
 
   it('Should render customized callout on mouseover', () => {
+    // Arrange
     wrapper = mount(
       <DonutChart
         data={chartPoints}
@@ -155,8 +193,170 @@ describe('DonutChart - mouse events', () => {
         }
       />,
     );
+
+    // Act
     wrapper.find('path[id^="_Pie_"]').at(0).simulate('mouseover');
+
+    // Assert
     const tree = toJson(wrapper, { mode: 'deep' });
     expect(tree).toMatchSnapshot();
+  });
+
+  describe('DonutChart - value inside donut handler event testing', () => {
+    beforeEach(sharedBeforeEach);
+    afterEach(sharedAfterEach);
+
+    it('Should render callout correctly on valueinside', () => {
+      // Arrange
+      wrapper = mount(<DonutChart data={chartPoints} innerRadius={55} valueInsideDonut={'Text inside'} />);
+
+      // Assert before Act
+      expect(wrapper.find('DonutChartBase').state('showHover')).toBe(false);
+
+      // Act
+      wrapper.find('DonutChartBase').setState({ activeLegend: 'first' });
+      wrapper.mount();
+      wrapper.find('path[id^="_Pie_"]').at(0).simulate('mouseover');
+
+      // Assert
+      expect(wrapper.find('DonutChartBase').state('showHover')).toBe(true);
+      const tree = toJson(wrapper, { mode: 'deep' });
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe('DonutChart - on dismiss handler event testing', () => {
+    beforeEach(sharedBeforeEach);
+    afterEach(sharedAfterEach);
+
+    it('Should set state correctly on blur', () => {
+      // Arrange
+      wrapper = mount(<DonutChart data={chartPoints} innerRadius={55} />);
+
+      // Act
+      wrapper.find('path[id^="_Pie_"]').at(0).simulate('blur');
+
+      //Assert
+      expect(wrapper.find('DonutChartBase').state('focusedArcId')).toBe('');
+      const tree = toJson(wrapper, { mode: 'deep' });
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('Should set state correctly on focus', () => {
+      // Arrange
+      wrapper = mount(<DonutChart data={chartPoints} innerRadius={55} />);
+
+      // Act
+      wrapper.find('path[id^="_Pie_"]').at(0).simulate('focus');
+
+      // Assert
+      expect(wrapper.find('DonutChartBase').state('showHover')).toBe(true);
+      expect(wrapper.find('DonutChartBase').state('legend')).toBe('first');
+      expect(wrapper.find('DonutChartBase').state('color')).toBe('#E5E5E5');
+      expect(wrapper.find('DonutChartBase').state('xCalloutValue')).toBe('2020/04/30');
+      expect(wrapper.find('DonutChartBase').state('yCalloutValue')).toBeUndefined();
+      expect(wrapper.find('DonutChartBase').state('callOutAccessibilityData')).toBeUndefined();
+      const tree = toJson(wrapper, { mode: 'deep' });
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('Should set state correctly on close callout', () => {
+      // Arrange
+      wrapper = mount(<DonutChart data={chartPoints} innerRadius={55} calloutProps={{ doNotLayer: true }} />);
+
+      // Act
+      wrapper.find('path[id^="_Pie_"]').at(0).simulate('focus');
+      const callout = wrapper.find('Callout').at(0);
+      callout.simulate('close');
+
+      //Assert
+      expect(wrapper.find('DonutChartBase').state('showHover')).toBe(true);
+      expect(wrapper.find('DonutChartBase').state('legend')).toBe('first');
+      expect(wrapper.find('DonutChartBase').state('color')).toBe('#E5E5E5');
+      expect(wrapper.find('DonutChartBase').state('xCalloutValue')).toBe('2020/04/30');
+      expect(wrapper.find('DonutChartBase').state('yCalloutValue')).toBeUndefined();
+      expect(wrapper.find('DonutChartBase').state('callOutAccessibilityData')).toBeUndefined();
+      const tree = toJson(wrapper, { mode: 'deep' });
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('Should set state correctly on mouse leave', () => {
+      // Arrange
+      wrapper = mount(<DonutChart data={chartPoints} innerRadius={55} />);
+
+      // Act
+      wrapper.find('DonutChartBase').first().at(0).simulate('mouseleave');
+
+      // Assert
+      expect(wrapper.find('DonutChartBase').state('showHover')).toBe(false);
+      const tree = toJson(wrapper, { mode: 'deep' });
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('Should behave correctly on mouse over on legends', () => {
+      // Arrange
+      wrapper = mount(<DonutChart data={chartPoints} innerRadius={55} hideLegend={false} />);
+
+      // Act
+      wrapper.find('LegendsBase').find('button').at(0).simulate('mouseover');
+
+      // Assert
+      const tree = toJson(wrapper, { mode: 'deep' });
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('Should set state correctly on mouse click on legends (path where selected legend state is not set)', () => {
+      // Arrange
+      wrapper = mount(<DonutChart data={chartPoints} innerRadius={55} hideLegend={false} />);
+
+      // Act
+      wrapper.find('LegendsBase').find('button').at(0).simulate('click');
+
+      // Assert
+      expect(wrapper.find('DonutChartBase').state('selectedLegend')).toBe('first');
+      const tree = toJson(wrapper, { mode: 'deep' });
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('Should set state correctly on mouse click on legends (path where selected legend state is set)', () => {
+      // Arrange
+      wrapper = mount(<DonutChart data={chartPoints} innerRadius={55} hideLegend={false} />);
+
+      // Act
+      wrapper.find('DonutChartBase').setState({ selectedLegend: 'first' });
+      wrapper.find('LegendsBase').find('button').at(0).simulate('click');
+
+      // Assert
+      expect(wrapper.find('DonutChartBase').state('selectedLegend')).toBe('');
+      const tree = toJson(wrapper, { mode: 'deep' });
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('Should set state correctly on mouse out of legends', () => {
+      // Arrange
+      wrapper = mount(<DonutChart data={chartPoints} innerRadius={55} hideLegend={false} />);
+
+      // Act
+      wrapper.find('LegendsBase').find('button').at(0).simulate('mouseout');
+
+      // Assert
+      expect(wrapper.find('DonutChartBase').state('activeLegend')).toBe('');
+      const tree = toJson(wrapper, { mode: 'deep' });
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('Should set the height and width correctly as given in props', () => {
+      // Arrange
+      wrapper = mount(<DonutChart data={chartPoints} innerRadius={55} height={200} width={600} />);
+
+      // Act
+      wrapper.find('path[id^="_Pie_"]').at(0).simulate('mouseover');
+
+      // Assert
+      expect(wrapper.find('DonutChartBase').state('_height')).toEqual(160);
+      expect(wrapper.find('DonutChartBase').state('_width')).toEqual(600);
+      const tree = toJson(wrapper, { mode: 'deep' });
+      expect(tree).toMatchSnapshot();
+    });
   });
 });
